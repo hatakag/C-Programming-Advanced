0fdbbf8e4e968d76cb068e9c54d84875b1235ed3
hatakag <thanhkbui2412@gmail.com>
2017-10-31 22:12:39 +0700

    test

--- week9/main.c
+++ week9/main.c
@@ -1,105 +1,210 @@
-#include <string.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-//#include "btree.h"
-//#include "jval.h"
-//#include "jrb.h"
-#define 
-#define
-
+#include "jval.h"
+#include "jrb.h"
 
 enum{SUCCESS,FAIL};
 
 typedef struct {
-  char ;
-  char ;
-};
+  char domain[100];
+  char ip[15];
+}dns;
 
 //FUNCTION
 
+void addEntry(JRB book,char* key,char* value) {
+  JRB node=jrb_find_str(book,key);
+  if (!node) {// if not exist insert new
+    char *s;
+    s=(char*)malloc(strlen(value)*sizeof(char));
+    strcpy(s,value);
+    jrb_insert_str(book,strdup(key),new_jval_s(s));// ip - domain
+  }
+  else {
+    printf("Domain name and IP already exist!!!\n");
+  }
+  //(node->val).l=value; //overwrite if already exists
+}
+
+void printBook(JRB book) {
+  if (jrb_empty(book)) printf("0 domain name and IP\n"); //if empty
+  else {
+    JRB node;
+    jrb_traverse(node,book) {
+      printf("%s : %s\n",jval_s(node->val),jval_s(node->key));
+    }
+  }
+}
+
+void freeBook(JRB book) {
+  if (!jrb_empty(book)) {
+    JRB node;
+    jrb_traverse(node,book) {
+      free((node->key).s);
+      //free((node->val).s);
+    }
+  }
+  jrb_free_tree(book);
+}
+
+int readBook(JRB book,FILE *fp) {
+  int i,n;
+  fscanf(fp,"%d%*c",&n);
+  //printf("%d\n",n);
+  if (n>=1000) {
+    printf("Can not scan number\n");
+    return 0;
+  }
+  dns *dnsarr;
+  dnsarr=(dns*)malloc(n*sizeof(dns));
+  for (i=0;i<n;i++) {
+    fscanf(fp,"%[^\n]%*c %[^\n]%*c",dnsarr[i].domain,dnsarr[i].ip);//__fpurge(stdin);
+    //printf("%s : %s\n",dnsarr[i].ip,dnsarr[i].domain);
+    addEntry(book,dnsarr[i].ip,dnsarr[i].domain);
+  }
+  free(dnsarr);
+  fclose(fp);
+  return n;
+}
+/*
+void writeBook(JRB book,FILE *fp1) {
+  if (jrb_empty(book)) {}//fprintf(fp1,"0 domain name and IP\n"); //if empty
+  else {
+    fprintf(fp1,"");
+    JRB node;
+    jrb_traverse(node,book) {
+      fprintf(fp1,"%s\n%s\n",jval_s(node->val),jval_s(node->key));
+    }
+  }
+  fclose(fp1);
+  }*/
+//
+
 int main(int argc, char* argv[]) {
-  if (argc!=4) {
+  if (argc!=2) {
     printf("Wrong number of input\n");
     return 0;
   }
   int reval=SUCCESS;
+
   FILE *fp;
-  if ((fp=fopen(argv[2],"r")==NULL)) {
-    printf("Cannot open file %s\n",argv[2]);
+  if ((fp=fopen(argv[1],"r"))==NULL) {
+    printf("Cannot open file %s\n",argv[1]);
     reval=FAIL;
   }
-  FILE * fp1;
-  if ((fp1=fopen(argv[3],"w"))==NULL) {
-    printf("Cannot open %s file\n",argv[2]);
-    reval=FAIL;
-  }
-
+  
   //red black tree
-  //JRB book=make_jrb();
-  
-  //btree
-  /*
-  BTA *book;
-  btinit();
-  if ((book=btopn(argv[1],0,0))!=NULL) {  // if file exists open B tree file
-    btdups(book,FALSE);
-    printf("Open file successfully\n");
-  } else if ((book=btcrt(argv[1],0,0))!=NULL) { //else create file
-    btdups(book,FALSE);
-  } else {
-    printf("Can't open file %s\n",argv[1]);
-    return 0;
-  }
-  */
-  
-  // read from file to btree
-  //readPhone(book,fp);
-  //fclose(fp);
+  JRB book=make_jrb();
 
+  
   //MENU 
-  char menu[6][40]={"Add","Find","Delete","Modify","Print","Quit"};
-  int choice;
+  char menu[5][40]={"Read file and Display","Add","Find IP","Find domain name","Quit"};
+  int choice,mark=0;
+  int N;
   do {
     choice=0;
-    printf("\n====PHONEBOOK====\n");
+    printf("\n=====DNS=====\n");
     printf("\n _____MENU_____\n");
     int i;
-    for (i=0;i<6;i++) 
+    for (i=0;i<5;i++) 
       printf("%d.%s\n",i+1,menu[i]);
     printf("Select: ");scanf("%d",&choice);__fpurge(stdin);
     switch (choice) {
     case 1: 
       {
+	if (mark==1) {
+	  printf("Already read file\n");
+	  printBook(book);
+	}
+	else {
+	  // read from file to btree
+	  N=readBook(book,fp);
+	  if (N==0) mark=0;
+	  else mark=1;
+	  printBook(book);
+	}
 	break;
       }
     case 2:
       {
+	if (mark==0) printf("Read file first\n");
+	else {
+	  char domain[100];
+	  char ip[15];
+	  printf("Domain: ");scanf("%[^\n]s%*c",domain);__fpurge(stdin);
+	  printf("IP: ");scanf("%[^\n]s%*c",ip);__fpurge(stdin);
+	  addEntry(book,ip,domain);
+	  //printBook(book);
+	  N++;
+	}
 	break;
       }
     case 3:
       {
+	if (mark==0) printf("Read file first\n");
+	else {
+	  if (jrb_empty(book)) printf("0 domain name and IP\n"); //if empty
+	  else {
+	    char ip[15];
+	    printf("IP: ");scanf("%[^\n]s%*c",ip);__fpurge(stdin);
+	    JRB node=jrb_find_str(book,ip);
+	    if (!node) {
+	      printf("Not found\n");
+	    } else {
+	      printf("%s : %s\n",jval_s(node->val),jval_s(node->key));
+	    }
+	  }
+	}
 	break;
       }
     case 4:
       {
+	if (mark==0) printf("Read file first\n");
+	else {
+	  if (jrb_empty(book)) printf("0 domain name and IP\n"); //if empty
+	  else {
+	    int check=0;
+	    char domain[100];
+	    printf("Domain: ");scanf("%[^\n]s%*c",domain);__fpurge(stdin);
+	    JRB node;
+	    jrb_traverse(node,book) {
+	      if (strcmp(jval_s(node->val),domain)==0){
+		printf("%s : %s\n",jval_s(node->val),jval_s(node->key));
+		check=1;
+	      }
+	    }
+	    if (check==0) printf("Not found\n");
+	  }
+	}
 	break;
       }
     case 5:
-      {
-	break;
-      }
-    case 6:
       break;
     default: printf("Invalid choice\n");
       break;
     }
-  }while(choice!=6);
+  }while(choice!=5);
   printf("\n___GOODBYE___\n");
       
   //write btree to file
-  //writePhone(book,fp1);
-  //fclose(fp1);
-  //btcls(book);
-  //freePhone(book);  //jrb
+
+  FILE *fp1;
+  if ((fp1=fopen(argv[1],"w"))==NULL) {
+    printf("Cannot open file %s\n",argv[1]);
+    reval=FAIL;
+  }
+  if (jrb_empty(book)) {}//fprintf(fp1,"0 domain name and IP\n"); //if empty
+  else {
+    fprintf(fp1,"%d\n",N);
+    JRB node;
+    jrb_traverse(node,book) {
+      fprintf(fp1,"%s\n%s\n",jval_s(node->val),jval_s(node->key));
+    }
+  }
+  fclose(fp1);
+  //writeBook(book,fp1);
+  
+  freeBook(book);  //jrb
   return reval;
 }
